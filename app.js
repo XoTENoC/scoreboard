3dd10125d24116252d07051f65e8f6ff423cc062
Nathaniel Chang <changipro.nathaniel@gmail.com>
2023-07-11 02:58:44 +0930

    adding persistent data and touch

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const fs = require('fs');
 
 // Create a new Express application
 const app = express();
@@ -25,19 +26,37 @@ app.use(session({
 
 // Set up the initial scores
 let scores = {
-	team1: 0,
-	team2: 0,
-	team3: 0,
-	team4: 0,
-	team5: 0,
-	team6: 0,
-	team7: 0,
-	team8: 0
+    team1: 0,
+    team2: 0,
+    team3: 0,
+    team4: 0,
+    team5: 0,
+    team6: 0,
+    team7: 0,
+    team8: 0
+};
+
+// Load scores from file on server start
+fs.readFile('scores.json', 'utf8', (err, data) => {
+    if (!err) {
+        scores = JSON.parse(data);
+    } else {
+        console.log('Error loading scores from file:', err);
+    }
+});
+
+// Save scores to file
+const saveScoresToFile = () => {
+    fs.writeFile('scores.json', JSON.stringify(scores), 'utf8', (err) => {
+        if (err) {
+            console.log('Error saving scores to file:', err);
+        }
+    });
 };
 
 app.get('/', (req, res) => {
-	// Render the scoreboard page with the current scores
-	res.render('scoreboard', { scores });
+    // Render the scoreboard page with the current scores
+    res.render('scoreboard', { scores });
 });
 
 app.get('/login', (req, res) => {
@@ -47,7 +66,7 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
     // Check if the submitted username and password match the ones stored in the app
-    if(req.body.username === 'controlboi' && req.body.password === 'applejuice') {
+    if (req.body.username === 'controlboi' && req.body.password === 'applejuice') {
         req.session.loggedIn = true;
         res.redirect('/control');
     } else {
@@ -56,7 +75,7 @@ app.post('/login', (req, res) => {
 });
 
 app.get('/control', (req, res) => {
-    if(req.session.loggedIn) {
+    if (req.session.loggedIn) {
         res.render('control', { scores });
     } else {
         res.redirect('/login');
@@ -65,56 +84,65 @@ app.get('/control', (req, res) => {
 
 
 app.post('/increase-scores', (req, res) => {
-	// Update the scores based on the button that was clicked
-	const { team, points } = req.body;
-	scores[team] += parseInt(points);
+    // Update the scores based on the button that was clicked
+    const { team, points } = req.body;
+    scores[team] += parseInt(points);
 
-	// Send a message to all connected clients with the updated scores
-	io.emit('score-update', scores);
+    // Save scores to file
+    saveScoresToFile();
 
-	// Send a response back to the client to confirm the update
-	res.send('Score updated!');
+    // Send a message to all connected clients with the updated scores
+    io.emit('score-update', scores);
+
+    // Send a response back to the client to confirm the update
+    res.send('Score updated!');
 });
 
 app.post('/decrease-scores', (req, res) => {
-	// Update the scores based on the button that was clicked
-	const { team, points } = req.body;
-	scores[team] -= parseInt(points);
+    // Update the scores based on the button that was clicked
+    const { team, points } = req.body;
+    scores[team] -= parseInt(points);
 
-	if(scores[team] < 0) {
-		scores[team] = 0;
-	}
+    if (scores[team] < 0) {
+        scores[team] = 0;
+    }
 
-	// Send a message to all connected clients with the updated scores
-	io.emit('score-update', scores);
+    // Save scores to file
+    saveScoresToFile();
 
-	// Send a response back to the client to confirm the update
-	res.send('Score updated!');
+    // Send a message to all connected clients with the updated scores
+    io.emit('score-update', scores);
+
+    // Send a response back to the client to confirm the update
+    res.send('Score updated!');
 });
 
 app.post('/reset-scores', (req, res) => {
-	teams = ['team1', 'team2', 'team3']
+    teams = ['team1', 'team2', 'team3'];
 
-	for(team in teams){
-		scores[teams[team]] = 0
-	}
+    for (team in teams) {
+        scores[teams[team]] = 0;
+    }
 
-	// Send a message to all connected clients with the updated scores
-	io.emit('score-update', scores);
+    // Save scores to file
+    saveScoresToFile();
 
-	// Send a response back to the client to confirm the update
-	res.send('Score updated!');
+    // Send a message to all connected clients with the updated scores
+    io.emit('score-update', scores);
+
+    // Send a response back to the client to confirm the update
+    res.send('Score updated!');
 });
 
 app.get("/scores", (req, res) => {
-	res.send(scores)
-})
+    res.send(scores);
+});
 
 app.get('*', (req, res) => {
-	res.redirect('/login');
+    res.redirect('/login');
 });
 
 // Start the server
 server.listen(3000, () => {
-	console.log('Server started on port 3000');
+    console.log('Server started on port 3000');
 });
