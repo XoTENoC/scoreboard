5ad16cd33747c95fb0b4a48b88d2c23710ae2790
Nathaniel <nathaniel@nuggslab.com>
2025-04-01 17:23:04 +1030

    changing port

--- app.js
+++ app.js
@@ -139,6 +139,6 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-server.listen(3000, () => {
-    console.log('Server started on port 3000');
+server.listen(6969, '0.0.0.0', () => {
+    console.log('Server started on port 6969');
 });
